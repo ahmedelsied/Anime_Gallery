d80585ee1d09feb841a0d21fb557342fbc4d04a8
ahmed elsied <43114893+ahmedelsied@users.noreply.github.com>
2020-08-30 20:10:50 +0200

    Update config.php

--- app/config.php
+++ app/config.php
@@ -5,7 +5,7 @@ if(!defined('DS')) {
 define('APP_PATH', realpath(dirname(__FILE__)) . '/' );
 define('TEMPENG' , APP_PATH.'template/');
 define('SERVER_NAME', 'https://'.$_SERVER['SERVER_NAME']);
-define('PUBLIC_PATH', 'https://'.$_SERVER['SERVER_NAME'].'/gallery/public/');
+define('PUBLIC_PATH', 'https://'.$_SERVER['SERVER_NAME'].'/gallery/');
 define('CSS',PUBLIC_PATH .'css/');
 define('JS',PUBLIC_PATH .'js/');
 define('IMG',PUBLIC_PATH .'img/');
